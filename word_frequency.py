7d0787b71b6fe67da654c7a9c6f62c89915de745
William Butts <Shea_Wolfe@Williams-MacBook-Pro.local>
2015-09-03 15:18:47 -0400

    removed non alphanums and split text into a list of words

--- word_frequency.py
+++ word_frequency.py
@@ -1,2 +1,8 @@
+import re
+test = 'This, is a Sample. Paragraph, for. some, basic testing yes yes yes yes yes.'
+
 def word_frequency(text):
-    return {"hello": 1}
+    trim_text = re.sub(r'[^a-zA-Z0-9\s]','' ,text).lower()
+    word_list = trim_text.split()
+    print(word_list)
+word_frequency(test)
