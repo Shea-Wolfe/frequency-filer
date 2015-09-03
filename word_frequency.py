bb5681dc04aa940e36dac69c85a43d1b4f227ebe
William Butts <Shea_Wolfe@Williams-MacBook-Pro.local>
2015-09-03 15:30:26 -0400

    have a dictionary function with word and count of that word in a string

--- word_frequency.py
+++ word_frequency.py
@@ -4,5 +4,8 @@ test = 'This, is a Sample. Paragraph, for. some, basic testing yes yes yes yes y
 def word_frequency(text):
     trim_text = re.sub(r'[^a-zA-Z0-9\s]','' ,text).lower()
     word_list = trim_text.split()
-    print(word_list)
+    word_count = {}
+    for word in word_list:
+        word_count[word] =  word_list.count(word)
+    return print(word_count)
 word_frequency(test)
