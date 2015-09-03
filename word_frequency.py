32257b27a2f0c4f2600ebb91b5f78c6b29f81e34
William Butts <Shea_Wolfe@Williams-MacBook-Pro.local>
2015-09-03 17:17:39 -0400

    mvp. Opens and reads sample.txt and outputs 20 most common words with count

--- word_frequency.py
+++ word_frequency.py
@@ -1,5 +1,4 @@
 import re
-test = 'This, is a Sample. Paragraph, for. some, basic testing yes yes yes yes yes.'
 
 def word_frequency(text):
     trim_text = re.sub(r'[^a-zA-Z0-9\s]','' ,text).lower()
@@ -7,5 +6,16 @@ def word_frequency(text):
     word_count = {}
     for word in word_list:
         word_count[word] =  word_list.count(word)
-    return print(word_count)
-word_frequency(test)
+    return word_count
+def main():
+    text = open('sample.txt')
+    read_text = text.read()
+    word_count = word_frequency(read_text)
+    word_count = sorted(word_count.items(), key=lambda c: c[1], reverse=True)
+    word_count = word_count[:20]
+    for pair in word_count:
+        print(pair)
+    text.close()
+
+if __name__ == '__main__':
+    main()
