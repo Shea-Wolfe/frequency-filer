43fdf9b4f01b3affabeed78d8f56520bf5dd2beb
William Butts <Shea_Wolfe@Williams-MacBook-Pro.local>
2015-09-03 20:57:31 -0400

    Updated README, added comments to word_frequency, added hard-mode attempt

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 ## Getting Started
-* to run test in command line use $ python word_frequency_test.py
-* to run program in command line use $ python copy word_frequency.py
+* To run test in command line use $ python word_frequency_test.py.
+* To run word_frequency in command line use $ python word_frequency.py.  This program requires a file named sample.txt to be present in the same directory.
+* To run hard_word_frequency in command line use $ python hard_word_frequency file_to_be_opened.txt.
 
-## The program
-This program will open an outside file and output a dictionary of the 20 most frequent words in the file.
+## The programs
+word_frequency will open an outside file in the same directory named sample.txt and print pairings of the 20 most common words in sample.txt and how many times they are used to standard output.
+
+word_frequency_tests checks the word_frequency() function in word_frequency.py and ensures it is properly counting each word and sorting them into a dictionary where key=word and value=count of that word.
+
+sample.txt is a file to be input into our word_frequency function.
+
+hard_word_frequency will open the text file indicated after you call hard_word_frequency and print out the 20 most common words excluding the words found below.
+
+excluded words:
+['a','able','about','across','after','all','almost','also','am','among','an','and','any','are','as','at','be',
+'because','been','but','by','can','cannot','could','dear','did','do','does','either','else','ever','every',
+'for','from','get','got','had','has','have','he','her','hers','him','his','how','however','i','if','in','into','is',
+'it','its','just','least','let','like','likely','may','me','might','most','must','my','neither','no','nor',
+'not','of','off','often','on','only','or','other','our','own','rather','said','say','says','she','should',
+'since','so','some','than','that','the','their','them','then','there','these','they','this','tis','to','too',
+'twas','us','wants','was','we','were','what','when','where','which','while','who','whom','why','will','with',
+'would','yet','you','your']
--- hard_word_frequency.py
+++ hard_word_frequency.py
@@ -1,9 +1,17 @@
-import re #used to strip non-text
+import re
+import sys #sys found on stack overflow
 
 def word_frequency(text):
-    '''takes a string and returns a dictionary of {word:count_of_word}'''
     trim_text = re.sub(r'[^a-zA-Z\s]','' ,text).lower()
     #trim_text is the string without formating or numbers but with spaces
+    del_words = ['a','able','about','across','after','all','almost','also','am','among','an','and','any','are','as','at','be',
+'because','been','but','by','can','cannot','could','dear','did','do','does','either','else','ever','every',
+'for','from','get','got','had','has','have','he','her','hers','him','his','how','however','i','if','in','into','is',
+'it','its','just','least','let','like','likely','may','me','might','most','must','my','neither','no','nor',
+'not','of','off','often','on','only','or','other','our','own','rather','said','say','says','she','should',
+'since','so','some','than','that','the','their','them','then','there','these','they','this','tis','to','too',
+'twas','us','wants','was','we','were','what','when','where','which','while','who','whom','why','will','with',
+'would','yet','you','your'] #can be any list of strings
     word_list = trim_text.split()
     #word_list splits the string into a list words
     word_count = {} #a dictionary to put our counts in
@@ -12,6 +20,12 @@ def word_frequency(text):
         #looks at every word in our list and sets it as
         #a key with a value equal to the count of that word
         #in the the list.  duplicates are overwritten.
+    for word in del_words:
+        try:
+            del word_count[word]
+        except:
+            pass
+        #iterates through our library and deletes any key that appears in our del_list
     return word_count
 
 def sorted_frequency(dictionary):
@@ -22,13 +36,14 @@ def sorted_frequency(dictionary):
     for pair in word_count:
         print(pair)
         #Iterates through our list and prints out each tuple on it's own line
-
 def main():
-    text = open('sample.txt') #opens the outside file
+    input_file = sys.argv[1] #sys and sys.argv found on stack overflow
+    text = open(input_file) #opens the file
     read_text = text.read() #reads the text file
     word_count = word_frequency(read_text) #runs the count function on the file
     sorted_frequency(word_count) #sorts the dictionary by count and prints the top 20 to stdout
     text.close() #closes the outside file
+
 
 if __name__ == '__main__':
     main()
